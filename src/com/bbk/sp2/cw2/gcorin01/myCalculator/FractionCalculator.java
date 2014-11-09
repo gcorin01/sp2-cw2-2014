5f704ea2200dc8083d67b0b700bdf092f9b443ca
Gabriel Corinaldesi <gcorin01@users.noreply.github.com>
2014-11-09 01:48:48 +0000

    New FractionCalculator and FractionCalculatorTest have been added to the myCalculator package.

---
+++ src/com/bbk/sp2/cw2/gcorin01/myCalculator/FractionCalculator.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.bbk.sp2.cw2.gcorin01.myCalculator;
+
+/**
+ * @author Gabriel
+ *
+ */
+public class FractionCalculator {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ src/com/bbk/sp2/cw2/gcorin01/myCalculator/FractionCalculatorTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.bbk.sp2.cw2.gcorin01.myCalculator;
+
+import org.junit.Test;
+
+/**
+ * @author Gabriel
+ *
+ */
+public class FractionCalculatorTest {
+    
+    @Test
+    static void testFractionCalulator() {
+        FractionCalculator calculator = new FractionCalculator();
+    }
+    
+
+}
